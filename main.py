9758dc1dd6072ed964a80cf855358435edda50c0
kimrobbin <156893524+kimrobbin@users.noreply.github.com>
2026-01-21 09:57:12 +0100

    Made it so i can use 1 image for the sprites of the cookies but is still a WIP

---
+++ main.py
@@ -0,0 +1,74 @@
+import pygame
+
+pygame.init()
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 500, 500
+
+window = pygame.display.set_mode([WINDOW_HEIGHT,WINDOW_WIDTH])
+pygame.display.set_caption("Cookie clicker")
+
+
+class Cookie:
+    def __init__(self, x_pos, y_pos,width, height):
+        self._x_pos = x_pos
+        self._y_pos = y_pos
+        self._width = width
+        self._height = height
+
+    def draw(self, cookie_level):
+        
+        # cookie = pygame.image.load(f"cookies/cookies_{cookie_level}.png")
+        # cookie_rect = cookie.get_rect()
+        # cookie_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+
+        # window.blit(cookie, cookie_rect)
+
+        original_cookie = pygame.image.load("cookies.png")
+        # Adjust x_pos based on cookie_level to get the next cookie
+        x_offset = self._x_pos + (cookie_level - 1) * self._width
+        cookie = original_cookie.subsurface(pygame.Rect(x_offset, self._y_pos, self._height, self._width))
+
+        cookie_rect = cookie.get_rect()
+        cookie_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 )
+        window.blit(cookie, cookie_rect)
+
+
+cookie = Cookie(18,25,250,250)
+
+
+cookie_level = 1
+
+
+font = pygame.font.SysFont('Arial', 32)
+
+upgrade_tekst_render = font.render("Upgrade cookie!", True, (0,0,0))
+upgrade_tekst_rect = upgrade_tekst_render.get_rect(center=(WINDOW_HEIGHT // 2, 50))
+upgrade_tekst_rect.center = (WINDOW_HEIGHT // 2, 50)
+
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+                if cookie_level < 9: 
+
+                        if upgrade_tekst_rect.collidepoint(event.pos):
+                            cookie_level +=1
+
+            
+
+
+    window.fill((255, 255, 255)) 
+    window.blit(upgrade_tekst_render,upgrade_tekst_rect)
+
+    cookie.draw(cookie_level)
+
+
+    
+    pygame.display.update()
+
+pygame.quit()
